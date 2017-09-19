1e94f252c03ce153453cb5e03428273a2bb7e072
Mingyuan Dong <md3487@columbia.edu>
2017-09-19 11:45:58 -0400

    first commit

---
+++ md3487/src/main/java/EECS6893/md3487/App.java
@@ -0,0 +1,13 @@
+package EECS6893.md3487;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
